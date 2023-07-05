8304d965da54a97e47bf1d6ee73664a5316251f8
Fazan4ik <jagannath2006@outlook.com>
2023-07-05 16:02:12 +0300

    Add project files.

---
+++ AppVM.cs
@@ -0,0 +1,110 @@
+﻿
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace PhoneListMVVM
+{
+  
+
+    // view model class
+    public class AppVM : INotifyPropertyChanged
+    {
+        private Phone? selectedPhone;
+
+        public ObservableCollection<Phone> Phones { get; set; }
+
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand(obj =>
+                  {
+                      Phone phone = new Phone();
+                      Phones.Insert(0, phone);
+                      SelectedPhone = phone;
+                  }));
+            }
+        }
+
+        private RelayCommand removeCommand;
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return removeCommand ??
+                    (removeCommand = new RelayCommand(obj =>
+                    {
+                        Phone phone = obj as Phone;
+                        if (phone != null)
+                        {
+                            Phones.Remove(phone);
+                            if (SelectedPhone == phone)
+                                SelectedPhone = null;
+                        }
+                    }));
+            }
+        }
+
+        public Phone? SelectedPhone
+        {
+            get { return selectedPhone; }
+            set
+            {
+                selectedPhone = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+
+        public AppVM()
+        {
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone { Title="iPhone 14", Company = "Apple", Price = 41499 },
+                new Phone { Title="Samsung Galaxy S22", Company = "Samsung", Price = 50299 },
+                new Phone { Title="Xiaomi 12", Company = "Xiaomi", Price = 25999 },
+                new Phone { Title="Samsung Galaxy Fold 4", Company = "Samsung", Price = 79999 }
+            };
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace PhoneListMVVM
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new AppVM();
+            // пример добавления элемента через команды: https://metanit.com/sharp/wpf/22.3.php
+        }
+    }
+}
